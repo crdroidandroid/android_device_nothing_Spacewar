c4cd0f7e8e8909d2516635638a4441725fcc5060
Yumi Yukimura <me.cafebabe@gmail.com>
2024-06-12 22:41:36 +0800

    spacewar: Setup DeviceAsWebcam
    Change-Id: Icf8e10cd21c4eef9a66af624971ffe9c4d39d992
    Signed-off-by: Pranav Vashi <neobuddy89@gmail.com>

--- device.mk
+++ device.mk
@@ -18,6 +18,9 @@ DEVICE_PACKAGE_OVERLAYS += \
     $(LOCAL_PATH)/overlay \
     $(LOCAL_PATH)/overlay-lineage
 
+PRODUCT_PACKAGES += \
+    DeviceAsWebcamResTarget \
+
 PRODUCT_ENFORCE_RRO_TARGETS := *
 
 # Soong namespaces
